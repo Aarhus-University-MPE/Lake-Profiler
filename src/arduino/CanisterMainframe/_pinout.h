3b3c1c47923918dfdc2d40890dd03012f01371a7
Mads Rosenhøj Jeppesen <madsrosenhoej@gmail.com>
2022-05-05 19:24:54 +0200

    Update _pinout.h

--- src/arduino/CanisterMainframe/_pinout.h
+++ src/arduino/CanisterMainframe/_pinout.h
@@ -12,17 +12,11 @@
   2021
 */
 
-// Heartbeat
-#define PI_BUOY_HRTBEAT 2
-#define PO_BUOY_HRTBEAT 3
-#define PI_INT_HRTBEAT  digitalPinToInterrupt(PI_BUOY_HRTBEAT)
-
-// Reset pin
-#define PI_BUOY_RST     RESET
-#define PO_BUOY_RST     4
-
 // Communication
-#define COM_SERIAL_PC   Serial
-#define COM_CH4         Serial1
-#define COM_CO2         Serial2
-#define COM_CH4         Serial3
+#define COM_SERIAL_PC Serial
+#define COM_CH4       Serial1
+#define COM_CO2       Serial2
+
+#define COM_BAR100    Serial3
+#define COM_LUM       A0
+#define COM_TEMP      A1
