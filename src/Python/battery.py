297999adfb56e7268d858f3b06997099b5657f9f
Mads Rosenhøj Jeppesen <madsrosenhoej@gmail.com>
2023-03-04 13:47:27 +0100

    Python Scripts Documentation and Cleanup

--- src/Python/battery.py
+++ src/Python/battery.py
@@ -4,18 +4,30 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 
-bashCommand = "curl -o \"D:/Projects/Lake-Profiler/data/LoRa/battery.csv\" \"https://prototype.asap-forecast.com/logs/ormstrup-au?filter=ttn-payload&back="
-bashCommand += "2d\""
+# Define duration going back
+duration = "3d"
 
+# Raw payload directory
+directory = os.getcwd() + "/data/LoRa/battery.csv"
+
+# Bash command to download full payload for specified duration
+bashCommand = "curl -o \"" + directory + \
+    "\" \"https://prototype.asap-forecast.com/logs/ormstrup-au?filter=ttn-payload&back=" + duration + "\""
+
+# Execute bash command
 os.system(bashCommand)
 
+# Import data
 data = pd.read_csv('./data/LoRa/battery.csv')
 
+# Initialize variables
 level = []
 voltage = []
 time = []
 
+# Extract payloads containing battery readings
 for column in data.columns:
+    # Get LoRa message timestamp
     if ("createdAt" in column):
         loraMsg = column.split('"')[1]
         timeMsg = loraMsg.split('.')
@@ -23,37 +35,46 @@ for column in data.columns:
         timeObject += timedelta(hours=2)
         time.append(timeObject)
 
+    # Payload data
     if ("payload_hex:" in column):
         loraMsg = column.split('"')[1]
         payload = loraMsg.split(' ')
         try:
+            # Battery reading message identifier
             if (payload[1] == "3"):
                 level.append(int('0x'+payload[2], 0))
                 voltage.append(int('0x'+payload[3], 0) / 10)
+            # Message was not battery reading, remove last payload time
             else:
                 time.pop()
+        # Empty data package, remove last payload time
         except:
             time.pop()
 
 
-# plotting the points
+# Plotting battery readings
 fig, ax = plt.subplots()
-# ax.plot(time, level)
-ax.plot(time, voltage)
+ax.plot(time, level)
+ax2 = ax.twinx()
+ax2.plot(time, voltage)
 
+# Plot data (x-axis) format
 myFmt = mdates.DateFormatter('%d/%m - %H:%M')
 ax.xaxis.set_major_formatter(myFmt)
-
 plt.gcf().autofmt_xdate()
 
+# Axis labels
 plt.xlabel('Time')
-plt.ylabel('Charge Level [%]')
+ax.set_ylabel('Charge Level [%]')
+ax2.set_ylabel('Battery Voltage [V]')
 plt.title('Battery Level (Estimate)')
-# plt.ylim(0, 101)
-plt.ylim(10, 14)
+
+# Axis limits
+ax.set_ylim(0, 101)         # 0 - 100%
+ax2.set_ylim(10, 15)        # 10 - 15V
+
 
 # ax.set_facecolor('xkcd:grey')
-
 # fig.patch.set_facecolor('xkcd:grey')
 # function to show the plot
 plt.show()
--- src/Python/payload.py
+++ src/Python/payload.py
@@ -5,22 +5,29 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 
-bashCommand = "curl -o \"D:/Projects/Lake-Profiler/data/LoRa/payload.csv\" \"https://prototype.asap-forecast.com/logs/ormstrup-au?filter=ttn-payload&back="
-bashCommand += "1d\""
+# Define duration going back
+duration = "5d"
 
+# Raw payload directory
+directory = os.getcwd() + "/data/LoRa/payload.csv"
+
+# Bash command to download full payload for specified duration
+bashCommand = "curl -o \"" + directory + \
+    "\" \"https://prototype.asap-forecast.com/logs/ormstrup-au?filter=ttn-payload&back=" + duration + "\""
+
+# Execute bash command
 os.system(bashCommand)
 
+# Import data
 data = pd.read_csv('./data/LoRa/payload.csv')
 
-payloadData = []
-voltage = []
-time = []
 
-
+# Function to parse a payload message
 def parsePayload(payload):
     # Payload Index
     index = parsePayloadInt(payload, 2)
 
+    # Sensor data
     ch4ConcentrationEstimate = parsePayloadInt(payload, 7)
     ch4Concentration = parsePayloadInt(payload, 11)
     co2RawValues = parsePayloadInt(payload, 15)
@@ -29,19 +36,21 @@ def parsePayload(payload):
     temperatures = parsePayloadInt(payload, 27)
     luminesence = parsePayloadInt(payload, 31, 2)
 
+    # Sample data
     sampleID = parsePayloadInt(payload, 34, 2)
     timeStamp = parsePayloadInt(payload, 36)
 
+    # Bundle data
     data = [index, ch4ConcentrationEstimate, ch4Concentration,
             co2RawValues, co2Concentration, depth, temperatures, luminesence, sampleID, timeStamp]
 
     return data
 
 
+# Extract integer from payload
 def parsePayloadInt(payload, startIndex, length=4):
     # Add hex identifier
     byteString = ['0x' + s for s in payload[startIndex:startIndex+length]]
-    # print(byteString)
 
     # Convert to byteframe
     frame = b""
@@ -54,13 +63,20 @@ def parsePayloadInt(payload, startIndex, length=4):
     return intValue
 
 
+# Initialize csv headers
 payloadDataFormatted = [["Index", "CH4 Estimate", "CH4 Concentration",
                          "CO2 Raw", "CO2 Concentration", "Depth", "Temperatures", "Luminesence", "Sample ID", "Timestamp"]]
 
 payloadDataFormatted.append(["-", "ppm x 1000", "ppm x 1000",
                              "ppm x 1000", "ppmv x 1000", "m x 1000", "deg C x 1000", "-", "-", "Unix ms"])
 
+# Initialize data arrays
+time = []
+payloadData = []
+
+# Extract payloads containing profiling packages
 for column in data.columns:
+    # Get LoRa message timestamp
     if ("createdAt" in column):
         loraMsg = column.split('"')[1]
         timeMsg = loraMsg.split('.')
@@ -68,28 +84,33 @@ for column in data.columns:
         timeObject += timedelta(hours=2)
         time.append(timeObject)
 
+    # Payload data
     if ("payload_hex:" in column):
         loraMsg = column.split('"')[1]
         payload = loraMsg.split(' ')
         try:
+            # Profiling payload message identifier
             if (payload[1] == "41"):
+                # Extract and format payload
                 payloadData.append(payload)
                 formattedData = parsePayload(payload)
                 # check for duplicates
                 if (formattedData[0] != payloadDataFormatted[payloadDataFormatted.__len__()-1][0]):
                     payloadDataFormatted.append(formattedData)
-                # level.append(int('0x'+payload[2], 0))
-                # voltage.append(int('0x'+payload[3], 0) / 10)
+
+            # Message was not Profiling payload, remove last payload time
             else:
                 time.pop()
+        # Empty data package, remove last payload time
         except:
             time.pop()
 
-# print(payloadDataFormatted)
 
+# Convert to numpy array
 dataArray = np.asarray(payloadData)
-pd.DataFrame(dataArray).to_csv('./data/LoRa/payloadExtract.csv', sep=';')
-
 dataArrayFormatted = np.asarray(payloadDataFormatted)
+
+# Convert to csv file and save
+pd.DataFrame(dataArray).to_csv('./data/LoRa/payloadExtract.csv', sep=';')
 pd.DataFrame(dataArrayFormatted).to_csv(
     './data/LoRa/payloadExtractFormatted.csv', sep=';')
